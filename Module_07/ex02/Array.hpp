e85b26d887821be4311dcca024e8c17ef70654a2
tghnx1 <kokorevm2012@gmail.com>
2025-05-09 16:47:58 +0200

    Module 7 finished

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -8,14 +8,79 @@ the following behavior and functions:
 #include <string>
 #include <cstdint>
 
-template <typename T> class Array
+class TooBigIteratorException : public std::exception
 {
 	public:
-		Array() : value(new T())
+		virtual const char* what() const throw()
+	{
+		return "you're trying to access the memory block you don't have an access 🤦🤦🤦🤦🤦";
+	}
+};
+
+template <typename T>
+class Array
+{
+	private:
+		T *arr;
+		unsigned int len;
+	public:
+	//actions:
+
+	unsigned int size()
+	{
+		return this->len;
+	}
+
+	//Othodox canonical form:
+		Array()
 		{
-			std::cout << "lol" << std::endl;
+			arr = nullptr;
+			len = 0;
 		};
-		T *value;
-		int kek;
+
+		Array(unsigned int n)
+		{
+			this->arr = new T[n];
+			this->len = n;
+			for (unsigned int i = 0; i < len; i++)
+				arr[i] = T();
+		};
+
+		Array(const Array& oldone)
+		{
+				this->len = oldone.len;
+				this->arr = new T[len];
+				for (unsigned int i = 0; i < this->len; i++)
+				{
+					this->arr[i] = oldone.arr[i];
+				}
+		};
+
+		~Array()
+		{
+			delete[] this->arr;
+		};
+
+		Array& operator=(const Array& oldone)
+		{
+			this->len = oldone.len;
+			delete[] this->arr;
+			this->arr = new T[len];
+			for (unsigned int i = 0; i < this->len; i++)
+			{
+				this->arr[i] = oldone.arr[i];
+			}
+			return *this;
+		}
+
+		T& operator[](int i)
+		{
+			if (static_cast<unsigned int>(i) >= this->len)
+			{
+				throw TooBigIteratorException();
+			}
+			return this->arr[i];
+		}
 
 };
+
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -3,6 +3,35 @@
 
 int main()
 {
-	Array arr(6);
-}
+	Array<std::string> k(3);
+	Array<int> l(2);
+	Array<int> m(l);
 
+	k[0] = "lol";
+
+	 l[0] = 4;
+	 m[0] = 3;
+	 Array<std::string> n(5);
+	 n = k;
+	 n[0] = "kek";
+
+	std::cout << "l: "<< l[0] << std::endl;
+	std::cout << "m: " <<  m[0] << std::endl;
+	std::cout << "k: "<< k[0] << std::endl;
+	std::cout << "n: "<< n[0] << std::endl;
+
+	std::cout << "l size: " << l.size() << std::endl;
+	std::cout << "m size: " << m.size() << std::endl;
+	std::cout << "k size: " << k.size() << std::endl;
+	std::cout << "n size: " << n.size() << std::endl;
+
+	n[2] = "lolos";
+	std::cout << "n[2]: " << n[2] << std::endl;
+	try {std::cout << "n[228]: " << n[228] << std::endl;}
+	catch (const std::exception& g)
+	{
+		std::cout << "failed because " << g.what() << std::endl;
+	}
+
+	return 0;
+}
