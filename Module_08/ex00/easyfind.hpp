9597026a3fc682ab326905937c386ad1c56149e9
tghnx1 <kokorevm2012@gmail.com>
2025-05-21 14:13:10 +0200

    Module 08 finished

---
+++ Module_08/ex00/easyfind.hpp
@@ -0,0 +1,10 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include "easyfind.tpp"
+#include <iostream>
+
+template <typename T>
+typename T::iterator easyfind(T myContainer, int value);
+
+#endif //EASYFIND_HPP
